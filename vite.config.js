e3e12d840df201939994f911b304b263647d4899
Ivo Pereira <ivoecpereira@gmail.com>
2023-01-11 19:58:02 +0000

    add: gitpod support + vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,29 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+
+import { execSync } from 'node:child_process';
+
+let extendedViteDevServerOptions;
+
+try {
+	const gitpodPortUrl = execSync(`gp url 5173`).toString().trim();
+
+	extendedViteDevServerOptions = {
+		hmr: {
+			protocol: 'wss',
+			host: new URL(gitpodPortUrl).hostname,
+			clientPort: 443
+		}
+	};
+} catch {
+	extendedViteDevServerOptions = {};
+}
+
 export default defineConfig({
+    server: {
+        ...extendedViteDevServerOptions
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
