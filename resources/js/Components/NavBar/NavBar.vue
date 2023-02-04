676bbc09b708db2c53fb8db51ae0f9e224e0d942
shenriquesdev <Unisergius@gmail.com>
2023-02-04 20:20:27 +0000

    DarkMode + Package.Json

--- resources/js/Components/NavBar/NavBar.vue
+++ resources/js/Components/NavBar/NavBar.vue
@@ -3,8 +3,11 @@
     import SunIcon from '@/Components/Svg/SunIcon.vue';
     import MoonIcon from '@/Components/Svg/MoonIcon.vue';
     import HamburgerIcon from '@/Components/Svg/HamburgerIcon.vue';
-    
-    
+    import { useDark, useToggle } from '@vueuse/core';
+
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+
     export default {
         data() {
             return {
@@ -13,16 +16,14 @@
                     { name: 'Discord', url: 'https://discord.gg/52apEBYQb2' },
                     { name: 'Reddit', url: 'https://reddit.com/r/devpt/' }
                 ],
-                dark: false
+                dark: isDark
             }
         },
         methods: {
-            toggleDark() {
-                this.dark = !this.dark
-            }
+            toggleDark
         },
         components: {
-            GitHubIcon, 
+            GitHubIcon,
             SunIcon,
             MoonIcon,
             HamburgerIcon
@@ -31,14 +32,17 @@
 </script>
 
 <template>
-    <nav class="navbar bg-base-100 lg:px-4">
+    <nav class="navbar lg:px-4">
         <div class="navbar-start">
-            <a class="btn btn-ghost normal-case text-lg hidden lg:block" href="/" ><img class="h-8 w-auto" :src="'images/logo-blue.svg'" /></a>
+            <a class="btn btn-ghost normal-case text-lg hidden lg:block" href="/" >
+                <img v-show="this.dark" class="h-8 w-auto" :src="'images/logo-blue-white.svg'" />
+                <img v-show="!this.dark" class="h-8 w-auto" :src="'images/logo-blue.svg'" />
+            </a>
             <div class="dropdown">
                 <label tabindex="0" class="btn btn-ghost lg:hidden">
                     <HamburgerIcon class="h-5 w-5" />
                 </label>
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52">
                     <li v-for="link in links" :key="link.id" ><a :href="link.url">{{ link.name }}</a></li>
                 </ul>
             </div>
@@ -47,15 +51,15 @@
             </a>
         </div>
         <div class="navbar-center">
-            <img class="block lg:hidden h-8 w-auto"  :src="'images/logo-mini-blue.svg'" /> 
+            <img class="block lg:hidden h-8 w-auto"  :src="'images/logo-mini-blue.svg'" />
         </div>
         <div class="navbar-end">
             <a class="btn btn-ghost btn-circle" href="https://github.com/devpt-org">
                 <GitHubIcon width="20" height="20" />
             </a>
-            <a class="btn btn-ghost btn-circle" @click="toggleDark">
-                <SunIcon v-show="this.dark" width="20" height="20" class="svg-inline--fa fa-moon fa-w-16" />
-                <MoonIcon v-show="!this.dark" width="20" height="20" class="svg-inline--fa fa-moon fa-w-16" />
+            <a class="btn btn-ghost btn-circle" @click="toggleDark()">
+                <SunIcon v-show="!this.dark" width="20" height="20" />
+                <MoonIcon v-show="this.dark" width="20" height="20" />
             </a>
         </div>
     </nav>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,6 +4,7 @@ import HeroLogo from '@/Components/HeroLogo.vue'
 import HomepageBenefits from "@/Components/HomepageBenefits.vue";
 import NavBar from '@/Components/NavBar/NavBar.vue'
 
+
 defineProps({
     benefits: Object,
 })
@@ -11,20 +12,28 @@ defineProps({
 
 <template>
     <Head title="devPT - Comunidade de Developers Portugueses"/>
-    <NavBar />
-    <section class="flex items-center">
-        <div>
-            <HeroLogo class="max-w-[400px] lg:max-w-[800px]"/>
-        </div>
-        <div class="text-white">
-            <div class="text-xl text-orange-500">Comunidade developers portugueses</div>
-            <div class="text-2xl font-[1100]">Partilha, participa e comunica.</div>
-            <div>Somos uma comunidade sem fins lucrativos com foco nos developers portugueses.</div>
-            <a class="btn text-white bg-blue-400 hover:bg-blue-600">Colabora</a>
-        </div>
-    </section>
-    <section>
-        <HomepageBenefits :benefits="benefits"/>
-    </section>
+        <NavBar />
+        <section class="flex items-center">
+            <div>
+                <HeroLogo class="max-w-[400px] lg:max-w-[800px]"/>
+            </div>
+            <div >
+                <div class="text-xl text-orange-500">Comunidade developers portugueses</div>
+                <div class="text-2xl font-[1100]">Partilha, participa e comunica.</div>
+                <div>Somos uma comunidade sem fins lucrativos com foco nos developers portugueses.</div>
+                <a class="btn text-white bg-blue-400 hover:bg-blue-600">Colabora</a>
+            </div>
+        </section>
+        <section>
+            <HomepageBenefits :benefits="benefits"/>
+        </section>
 
 </template>
+
+<style>
+
+    body {
+        @apply bg-white dark:bg-zinc-800 text-black dark:text-white;
+    }
+
+</style>
