290b90e8d80dceaf9fc099dfcfd76ef0f0cc4fbc
shenriquesdev <Unisergius@gmail.com>
2023-02-25 18:03:11 +0000

    Layout Footer apart

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,21 +1,13 @@
 <script setup>
     import {Head, Link} from '@inertiajs/inertia-vue3';
-    import NavBar from '@/Components/NavBar/NavBar.vue'
+    import NavBar from '@/Components/NavBar/NavBar.vue';
+    import Footer from '@/Components/Footer/Footer.vue';
+
 </script>
 
 <template>
     <Head title="devPT - Comunidade de Developers Portugueses"/>
     <NavBar />
     <slot />
-    <Footer class="w-full text-left p-4 pin-b text-xs bg-sky-600 text-white font-bold">
-                <p>
-                    made with ♡
-                    <a
-                        href="https://devpt-org.github.io/"
-                        class="underline  hover:text-blue-400"
-                        >
-                        by devPT
-                    </a>
-                </p>
-    </Footer>
+    <Footer />
 </template>
